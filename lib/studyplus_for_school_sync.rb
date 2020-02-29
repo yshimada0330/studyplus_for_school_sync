86327cb3cb3690adfb42d21bdd013619a196850b
shimada <shimada227@gmail.com>
2020-02-29 23:53:08 +0900

    add Client#post

--- lib/studyplus_for_school_sync.rb
+++ lib/studyplus_for_school_sync.rb
@@ -1,6 +1,9 @@
 require "studyplus_for_school_sync/version"
 
+require "jsonclient"
+
+require "studyplus_for_school_sync/client"
+
 module StudyplusForSchoolSync
   class Error < StandardError; end
-  # Your code goes here...
 end
--- lib/studyplus_for_school_sync/client.rb
+++ lib/studyplus_for_school_sync/client.rb
@@ -1,12 +1,16 @@
 module StudyplusForSchoolSync
   class Client
-    def initialize
+    def initialize(base_url:)
+      @base_url = base_url
+      @client = JSONClient.new
     end
 
     def get
     end
 
-    def post
+    def post(access_token:, path:, params:)
+      headers = { 'Authorization' => "Bearer #{access_token}",'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      @client.post("#{@base_url}/#{path}", body: params, header: headers)
     end
 
     def patch
