fa27a6e33402e4f6582f8d164673c047d1f17f5e
shimada <shimada227@gmail.com>
2020-03-01 15:47:14 +0900

    add LearningMaterialSupplier#create_partner

--- lib/studyplus_for_school_sync/endpoint/learning_material_supplier.rb
+++ lib/studyplus_for_school_sync/endpoint/learning_material_supplier.rb
@@ -3,10 +3,13 @@ module StudyplusForSchoolSync
     module LearningMaterialSupplier
       BASE_PAH = "learning_material_supplier_api/v1"
 
+      def create_partner(school_name:, **options)
+        post(path: "#{BASE_PAH}/partners", params: options.merge(school_name: school_name))
+      end
+
       def create_learning_material(name:, **options)
         post(path: "#{BASE_PAH}/learning_materials", params: options.merge(name: name))
       end
-
 
       def update_learning_material(learning_material_public_id:, name:, **options)
         put(path: "#{BASE_PAH}/learning_materials/#{learning_material_public_id}", params: options.merge(name: name))
