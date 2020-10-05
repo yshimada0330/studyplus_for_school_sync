d3317971b9b949ca619ab73cb8c7919fa8cb5900
gaaamii <i1shi4ga4mi1@gmail.com>
2020-10-05 16:27:39 +0900

    認可Code取得のURLを叩く

--- lib/studyplus_for_school_sync/authorizer.rb
+++ lib/studyplus_for_school_sync/authorizer.rb
@@ -1,10 +1,24 @@
 module StudyplusForSchoolSync
   class Authorizer
+    OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
     def initialize(client_id: nil, redirect_uri: nil)
       @client_id = client_id || ENV["CLIENT_ID"]
       @redirect_uri = redirect_uri || ENV["REDIRECT_URI"]
       @response_type = "code"
       @scope = "learning_material_supplier"
+      @client = Faraday.new
+    end
+
+    def authorize
+      path = "learning_material_supplier_api/v1/oauth/authorize"
+      @client.get(build_url(path))
+    end
+
+    private
+
+    def build_url(path)
+      URI.join(ENV["BASE_URL"], path)
     end
   end
 end
