00316a7c147e6706c95cbcf431257dd2ca087ae9
shimada <shimada227@gmail.com>
2021-05-19 10:01:23 +0900

    open authorize url

--- lib/studyplus_for_school_sync.rb
+++ lib/studyplus_for_school_sync.rb
@@ -5,6 +5,7 @@ require "faraday"
 require "studyplus_for_school_sync/endpoint"
 require "studyplus_for_school_sync/client"
 require "studyplus_for_school_sync/server"
+require "studyplus_for_school_sync/authorizer"
 
 module StudyplusForSchoolSync
   class Error < StandardError; end
