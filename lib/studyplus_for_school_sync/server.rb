caae341d0ca10aea4e05c95e5c0730a29326acc1
shimada <shimada227@gmail.com>
2021-05-19 11:25:08 +0900

    redirect to Authorization Code

--- lib/studyplus_for_school_sync/server.rb
+++ lib/studyplus_for_school_sync/server.rb
@@ -1,13 +1,27 @@
 require "webrick"
+require "webrick/https"
+require "erb"
 
 module StudyplusForSchoolSync
   class Server
     def start
-      root = File.expand_path("../html/index.html", __FILE__)
+      root = File.expand_path("../html/index.erb", __FILE__)
 
-      server = WEBrick::HTTPServer.new Port: 8080, DocumentRoot: root
+      server = WEBrick::HTTPServer.new(
+        Port: 8080,
+        DocumentRoot: ".",
+        SSLEnable: true,
+        SSLCertName: [["CN", WEBrick::Utils.getservername]],
+      )
+
+      WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
+      server.config[:MimeTypes]["erb"] = "text/html"
+      server.mount_proc("/") { |req, res|
+        code = req.query["code"]
+        template = ERB.new(File.read(root))
+        res.body << template.result(binding)
+      }
       trap "INT" do server.shutdown end
-
       server.start
     end
   end
