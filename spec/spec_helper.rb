5543bd3ce2290785ae421fa02f807d5117b960aa
shimada <shimada227@gmail.com>
2021-05-30 00:03:34 +0900

    use simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require "bundler/setup"
 require "studyplus_for_school_sync"
+require 'simplecov'
+
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
